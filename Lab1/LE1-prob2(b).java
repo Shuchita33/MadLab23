9c2acc7ebff23319c047dc4b6e4b7615f4ccb11c
Shuchita_33 <100029047+Shuchita33@users.noreply.github.com>
2023-05-13 23:25:58 +0530

    Add files via upload

---
+++ Lab1/LE1-prob2(b).java
@@ -0,0 +1,20 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+public class Main
+{
+	public static void main(String[] args) {
+	    int n=Integer.parseInt(args[0]);
+	    for(int i=0;i<=n-1;i++){
+	        for(int j=0;j<i;j++) System.out.print(" ");
+	        for(int k=0;k<=n-1-i;k++) System.out.print("* ");
+	        System.out.println(" ");
+	    }
+	}
+}
+
